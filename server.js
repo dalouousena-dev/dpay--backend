8f94a43dd1df32aebffe10773259e6393e1352a7
dalouousena-dev <dalouousena@gmail.com>
2026-03-14 15:13:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -22,8 +22,6 @@ const PORT = process.env.PORT || 5000;
 
 /* ===== MIDDLEWARE ===== */
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 /* ===== SUPABASE INITIALIZATION ===== */
 const supabaseUrl = process.env.SUPABASE_URL;
